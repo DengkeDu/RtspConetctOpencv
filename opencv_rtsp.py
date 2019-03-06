045e1400c56d10b63552fe2b936fcee8d11c002a
dudengke <pinganddu90@gmail.com>
2019-03-06 13:11:22 +0800

    take picture from the remote video
    Signed-off-by: dudengke <pinganddu90@gmail.com>

--- opencv_rtsp.py
+++ opencv_rtsp.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import sys
 
-cap = cv.VideoCapture("rtspsrc location=rtsp://128.224.176.161:8554/test latency=0 ! rtph264depay ! decodebin ! videoconvert ! appsink")
+cap = cv.VideoCapture("rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 ! rtph264depay ! decodebin ! videoconvert ! appsink")
 
 if cap.isOpened() == False:
     print "VideoCapture Failed!"
@@ -13,10 +13,14 @@ while(True):
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Display the resulting frame
-    cv.imshow('frame',gray)
     cv.imshow('ori',frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    c = cv.waitKey(1)
+    if c == ord('q'):
         break
+    elif c == ord('p'):
+        print "Saved picture image.png"
+        cv.imwrite('image.png',frame)
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
